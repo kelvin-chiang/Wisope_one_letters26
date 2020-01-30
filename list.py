53cc4b28122dc35f1af00c7cfabddca60f515024
kelvin-chiang <752025767@qq.com>
2020-01-31 04:02:08 +0800

    26 letters

---
+++ list.py
@@ -0,0 +1,32 @@
+# list 26 letters
+letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+# print the 26 Capital letters
+print(
+letters[0].title(),
+letters[1].title(),
+letters[2].title(),
+letters[3].title(),
+letters[4].title(),
+letters[5].title(),
+letters[6].title(),
+letters[7].title(),
+letters[8].title(),
+letters[9].title(),
+letters[10].title(),
+letters[11].title(),
+letters[12].title(),
+letters[13].title(),
+letters[14].title(),
+letters[15].title(),
+letters[16].title(),
+letters[17].title(),
+letters[18].title(),
+letters[19].title(),
+letters[20].title(),
+letters[21].title(),
+letters[22].title(),
+letters[23].title(),
+letters[24].title(),
+letters[25].title()
+)
+
